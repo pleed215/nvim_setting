3a833a44df323e0ba051b8f800e86eabbc301e9b
Eun Deok, Lee <pleed0215@gmail.com>
2023-03-16 00:14:17 +0900

    pylsp added on mason

--- lua/pleed0215/plugins/lsp/mason.lua
+++ lua/pleed0215/plugins/lsp/mason.lua
@@ -1,44 +1,40 @@
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-    return
+	return
 end
 
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-    return
+	return
 end
-
 
 local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
 if not mason_null_ls_status then
-    return
+	return
 end
-
-
 
 mason.setup()
 
 -- https://github.com/williamboman/mason-lspconfig.nvim
 -- https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
 mason_lspconfig.setup({
-    ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "lua_ls",
-        "marksman",
-        "rust_analyzer",
-    },
+	ensure_installed = {
+		"tsserver",
+		"html",
+		"cssls",
+		"tailwindcss",
+		"lua_ls",
+		"marksman",
+		"pylsp",
+		"rust_analyzer",
+	},
 })
-
-
 
 -- https://github.com/jay-babu/mason-null-ls.nvim
 mason_null_ls.setup({
-    ensure_installed = {
-        "prettier",
-        "stylua",
-        "eslinst_d",
-    }
+	ensure_installed = {
+		"prettier",
+		"stylua",
+		"eslinst_d",
+	},
 })
